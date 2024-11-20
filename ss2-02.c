3b19862d9a86e1261cd6e9cfa5d164b9a59aa63d
phuonglinhle268 <plinhle26@gmail.com>
2024-11-21 00:50:24 +0700

    ss2-02

---
+++ ss2-02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int firstNumber = 3;
+	int secondNumber = 10;
+	int sum = fisrtNumber + secondNumber;
+	printf("Tong cua %d và %d là: %d\n", firstNUmber, secondNumber, sum); 
+	 
+	return 0; 
+}
+
+   
