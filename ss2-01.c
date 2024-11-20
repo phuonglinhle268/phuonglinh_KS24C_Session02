33c7018d4deab4f1c53bec8a9149dcef02ca0976
phuonglinhle268 <plinhle26@gmail.com>
2024-11-21 00:50:02 +0700

    ss2-01

---
+++ ss2-01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	//Kieu du lieu int (so nguyen)
+	int a = 3
+	//Kieu du lieu short
+	short b = 1
+	//Kieu du lieu long
+	long c = 8475083
+	//Kieu du lieu float ( so thuc, so thap phan)
+	float d = 3.14
+	//Kieu du lieu double
+	double e = 6.28
+	//Kieu du lieu boolean (nhan gia tri TRUE hoac FALSE)
+	_Bool f = 0
+	//In gia tri cac bien ra man hinh
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	printf("c = %d\n", c);
+	printf("d = %d\n", d);
+	printf("e = %d\n", e);
+	printf("f = %d\n", f);
+	
+	return 0
+}
