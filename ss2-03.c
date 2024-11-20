00e9e383ff821931fde00295eaf68af6ca106f11
phuonglinhle268 <plinhle26@gmail.com>
2024-11-21 00:50:44 +0700

    ss2-03

---
+++ ss2-03.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int firstNumber = 3;
+	int secondNumber = 10;
+	
+	sum = fisrtNumber + secondNumber;
+	minus = firstNumber - secondNumber;
+	devide = firsNumber / secondNumber;
+	multy = firstNumber * secondNumber;  
+	
+	printf("Tong cua 2 so la : %d\n", firstNUmber, secondNumber, sum); 
+	printf("Hieu cua 2 so la : %d\n", firstNumber, secondNumber, minus);
+	printf("Tich cua 2 so la : %d\n", fisrtNumber, secondNumber, multy);
+	printf("Thuong cua 2 so la : %f\n", firstNumber, secondNumber, devide);
+	
+	return 0; 
+}
+
+   
